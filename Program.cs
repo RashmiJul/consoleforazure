3e2656ff56ff486d942ede99874daa4184e74b2c
Rashmi J <rashmi_juluru@outlook.com>
2021-10-21 10:59:04 +0530

    created app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleazure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World edited!");
+        }
+    }
+}
